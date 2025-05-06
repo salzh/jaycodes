a74b43aa889e2c18a5a9ae3ecacf2b331fc2899e
salzh <zhongxiang721@163.com>
2025-05-06 15:39:51 +0800

    u

--- bin/app_konference.listener.pl
+++ bin/app_konference.listener.pl
@@ -186,6 +186,8 @@ sub process_queuecallback_request() {
 	local $callback_number = $event{CallbackNum};
 	
 	warn "Get callback request from callback_number=$callback_number on queue=$queue with position=$position";
+	local $response = &asterisk_manager_command('Action' => 'originate', 'Channel' => "Local/cb$callback_number/n", 'Context' => 'custom-queuecallback', 'Exten' => 'robot', 'Priority' => '1');
+	
 }
 
 sub command {
